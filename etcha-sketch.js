5885b30b2e3ab59d3bc043896a0e38b6f32251a7
MOGAR <alicea,9a@gmail.com>
2024-07-04 10:04:12 -0600

    created a grid of 16x16 squares that fill the whole page.

--- etcha-sketch.js
+++ etcha-sketch.js
@@ -2,9 +2,12 @@ let container = document.querySelector('#container');
 
 let square_amount = 256;
 
-/*let createGrid = (square_amount) => {
+let createGrid = (square_amount) => {
     for(let i = 0; i < square_amount; i++){
     let square = document.createElement('div');
+    square.setAttribute('id','grid');
     container.appendChild(square);
     };
-};*/
+};
+
+createGrid(square_amount);
