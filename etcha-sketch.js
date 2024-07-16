83eaca9a249e37c492ba84ed738b101689cc627b
MOGAR <alicea,9a@gmail.com>
2024-07-16 11:02:58 -0600

    Grid now changes to user input with no noticeable issues. Next is to fix/readd color changing

--- etcha-sketch.js
+++ etcha-sketch.js
@@ -1,35 +1,43 @@
 let container = document.querySelector('#container');
+let input = document.querySelector('input');
+let blocks = document.querySelectorAll('#block');
+Array.from(blocks);
 
-let grid_Sides = 16;
+
 
 let createGrid = (grid_Sides) => {
     for(let i = 0; i < grid_Sides; i++){
     let line = document.createElement('div');
     line.setAttribute('id','line');
+    
     for(let j = 0; j < grid_Sides;j++){
     let block = document.createElement('div')
     block.setAttribute('id','block')
-    //block.textContent = j;
     line.appendChild(block);
     };
+
     container.appendChild(line);
     };
 };
 
-createGrid(grid_Sides);
 
-let blocks = document.querySelectorAll('#block');
-Array.from(blocks);
-
-blocks.forEach((block) => {
-block.addEventListener('mouseover', () =>{
-    block.setAttribute('style','background-color:blue;');
-});
-});
-
-let reset = document.querySelector('button');
+let reset = document.querySelector('#reset');
 reset.addEventListener('click', ()=>{
-    blocks.forEach((block)=>{
-        block.setAttribute('style','background-color:white;');
-    });
+    container.textContent = ''
+    grid_Sides = input.value;
+    input.value = '';
+    createGrid(grid_Sides);
+
+
+});
+
+let start = document.querySelector('#start');
+start.addEventListener('click',()=>{
+
+    start.setAttribute('style','display:none;');
+    reset.setAttribute('style','display:block;');
+    grid_Sides = input.value;
+    input.value = '';
+    createGrid(grid_Sides);
+
 });
