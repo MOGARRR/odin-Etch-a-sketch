ab713ce93e97f90a5f9286e065ba21166ede4efc
MOGAR <alicea,9a@gmail.com>
2024-07-17 11:36:58 -0600

    input now only accepts numbers 1-100! next up extra credits!!

--- etcha-sketch.js
+++ etcha-sketch.js
@@ -31,23 +31,30 @@ let createGrid = (grid_Sides) => {
 
 let reset = document.querySelector('#reset');
 reset.addEventListener('click', ()=>{
+    if(input.value > 100 || input.value < 1){
+    alert('Please enter a number from 1-100');
+    return input.value ='';
+        
+    }else{
     container.textContent = ''
     grid_Sides = input.value;
     input.value = '';
     createGrid(grid_Sides);
 
-
+    }
 });
 
 let start = document.querySelector('#start');
 start.addEventListener('click',()=>{
-
+    if(input.value > 100 || input.value < 1){
+    alert('Please enter a number from 1-100');
+    return input.value ='';
+        
+    }else{
     start.setAttribute('style','display:none;');
     reset.setAttribute('style','display:block;');
     grid_Sides = input.value;
     input.value = '';
     createGrid(grid_Sides);
-
+    }
 });
-
-console.log(blocks);
